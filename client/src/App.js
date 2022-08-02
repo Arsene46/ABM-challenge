8a43291b8c31f54958472435a7986a93b9edde10
Arsene <arseneblavier46@gmail.com>
2022-08-02 12:36:21 -0300

    project template

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import s from "./LandingPage.module.css";
+
+export default function LandingPage() {
+  return <div className={s.landing}>Hello</div>;
+}
